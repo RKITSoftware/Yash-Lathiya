7b99c53b8f3b919a8d107d46ac3039ca9e11c724
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-08-08 00:16:45 +0530

    jQuery demo updated

--- Demo/GUI Basic Demo/6_jQuery/BillingSoftware.js
+++ Demo/GUI Basic Demo/6_jQuery/BillingSoftware.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+  //Store billNumber in local storage
+  if (localStorage.billNumber) {
+    console.log("Bill Number :" + localStorage.billNumber);
+  } else {
+    localStorage.billNumber = 1001;
+  }
+  $("#billNumberId").val(localStorage.billNumber);
+
   //Initialize Total Amount
   var totalAmount = 0.0;
   $("#totalAmount").val(Number(totalAmount).toFixed(2));
@@ -14,7 +22,22 @@ $(document).ready(function () {
     $("#itemAmount").val(amount);
   });
 
-  console.log($("#itemPrice").val());
+  //Reset Items
+  $("#btnResetItems").click(function () {
+    //Remove table Content
+    $("#tableItemDetails tr:nth-child(2)").nextAll().remove();
+    //Reset SrNumber
+    srNumber = 1;
+    $("#srNo").val(srNumber);
+    //Reset Form values
+    $("#itemName").val("");
+    $("#itemPrice").val("");
+    $("#itemQuantity").val("");
+    $("#itemAmount").val("");
+    //Reset Total Amount
+    totalAmount = 0.0;
+    $("#totalAmount").val(Number(totalAmount).toFixed(2));
+  });
 
   //Add item details in new row
   $("#btnAddItem").click(function () {
@@ -23,6 +46,19 @@ $(document).ready(function () {
     const itemPrice = Number($("#itemPrice").val()).toFixed(2);
     const itemQuantity = Number($("#itemQuantity").val()).toFixed(2);
     const itemAmount = Number($("#itemAmount").val()).toFixed(2);
+    //Blank fields are not allowed
+    if (itemName.trim() === "") {
+      alert("Enter Item Name");
+      return false;
+    }
+    if (itemPrice.trim() == 0.0) {
+      alert("Enter Item Price");
+      return false;
+    }
+    if (itemQuantity.trim() == 0.0) {
+      alert("Enter Item Quantity");
+      return false;
+    }
     var tableRow =
       "<tr><td>" +
       srNo +
@@ -42,5 +78,11 @@ $(document).ready(function () {
     //Update srNumber
     srNumber += 1;
     $("#srNo").val(srNumber);
+  });
+
+  //Confirm Bill Details
+  $("#btnConfirm").click(function () {
+    localStorage.billNumber = Number(localStorage.billNumber) + 1;
+    $("#billNumberId").val(localStorage.billNumber);
   });
 });
