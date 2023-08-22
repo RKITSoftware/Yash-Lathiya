1bea4a11e0a010bb3842782cc88b5f6ab6f6fdce
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-08-22 22:54:47 +0530

    C# Statements added

---
+++ Demo/API Basic Demo/1_Basics_of_C#/4Statements/4Statements/Program.cs
@@ -0,0 +1,46 @@
+﻿//Statements
+
+//Declaration Statements
+
+using System.Reflection.Emit;
+
+string firstName = "Yash";
+string lastName;
+const long aadharCardNumber = 382467184713;
+
+//Expression Statements
+
+lastName = "Lathiya";
+Console.WriteLine();
+
+//Goto Statements
+
+void additionalMethod()
+{
+    if (true) goto label;
+
+    label:
+    ; //Empty Statement
+}
+
+//Embedded Statements
+
+for(int i = 0; i< 10; i++)
+{
+    //Statements within { } block --> Embedded Statements
+}
+
+//Nested Statements
+
+for(int i = 0;i< 10; i++)
+{
+    for(int j = 0;j< i; j++)
+    {
+        Console.Write("*");
+        if(i == j) // always i > j 
+        {
+            Console.Write("Will Never Print"); //Unreachable Statement
+        }
+    }
+    Console.WriteLine();
+}
