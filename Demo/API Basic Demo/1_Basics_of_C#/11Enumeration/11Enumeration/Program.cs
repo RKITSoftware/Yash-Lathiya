5888dacae5c965001b22282a2929bf7db7538107
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2023-09-01 17:37:14 +0530

    C# enumeration added

---
+++ Demo/API Basic Demo/1_Basics_of_C#/11Enumeration/11Enumeration/Program.cs
@@ -0,0 +1,49 @@
+﻿//Enumeration is special class which contains constants..
+class Enumeration
+{
+    //Declare Enum
+    enum Days{
+        Monday,     //0
+        Tuesday,    //1 
+        Wednesday,  //2
+        Thursday,   //3
+        Friday,     //4
+        Saturday,   //5
+        Sunday      //6
+    }
+
+    enum Currency{
+        Rupees = 100,
+        Doller,
+        Yen = 500,
+        Dehram
+    }
+
+    static void Main(string[] args)
+    {
+        //Accessing Enum Values
+
+        Console.WriteLine(Days.Monday);
+        Console.WriteLine((int)Days.Tuesday);
+
+        Console.WriteLine(Currency.Rupees);
+        Console.WriteLine((int)Currency.Dehram);
+
+        //Traversing Enum
+
+        //here enumItem is string 
+        foreach(string enumItem in Enum.GetNames(typeof(Currency)))
+        {
+            Console.WriteLine(enumItem);
+        }
+
+        //here enumItem is constant itself
+        foreach (Currency enumItem in Enum.GetValues(typeof(Currency)))
+        {
+            Console.WriteLine(enumItem + " = " + (int)enumItem);
+        }
+
+        //Unchangeble --> throws error
+        //Currency.Yen = 602;
+    }
+}
