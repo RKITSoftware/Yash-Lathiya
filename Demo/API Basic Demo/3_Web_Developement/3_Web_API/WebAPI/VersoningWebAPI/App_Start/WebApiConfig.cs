1bb265b0d42e1a0cb514ac4fe14e79f2e3f7b15a
yalathiya <yashlathiya59ppsv@gmail.com>
2024-01-12 12:21:22 +0530

    Query String Versioning

--- Demo/API Basic Demo/3_Web_Developement/3_Web_API/WebAPI/VersoningWebAPI/App_Start/WebApiConfig.cs
+++ Demo/API Basic Demo/3_Web_Developement/3_Web_API/WebAPI/VersoningWebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using Microsoft.Web.Http;
+using VersoningWebAPI.CustomControllSelector;
 
 namespace VersoningWebAPI
 {
@@ -9,23 +13,37 @@ namespace VersoningWebAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            
             // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             // API Version 1
-            config.Routes.MapHttpRoute(
-                name: "ApiVersion1",
-                routeTemplate: "api/v1/student1",
-                defaults : new { controller = "STStudentV1"}
-            );
+            //config.Routes.MapHttpRoute(
+            //    name: "ApiVersion1",
+            //    routeTemplate: "api/v1/student1",
+            //    defaults: new { controller = "STStudentV1" }
+            //);
 
             // API Version 2
+            //config.Routes.MapHttpRoute(
+            //    name: "ApiVersion2",
+            //    routeTemplate: "api/v1/student2",
+            //    defaults: new { controller = "STStudentV2" }
+            //);
+
+            //Query String Parameter
+
+            // Replace the customController with the default httpControllerselector of the web api
+            config.Services.Replace(typeof(IHttpControllerSelector),
+                                    new CustomControllerSelector(config));
+
+            // Routing path of the customControllerSelector
             config.Routes.MapHttpRoute(
-                name: "ApiVersion2",
-                routeTemplate: "api/v1/student2",
-                defaults: new { controller = "STStudentV2" }
+                name: "DefaultRoute",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
             );
         }
     }
