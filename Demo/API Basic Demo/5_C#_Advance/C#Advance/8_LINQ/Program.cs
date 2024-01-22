762ac0e9c4c989bfa3969fc25041fab87e6f747d
yalathiya <yashlathiya59ppsv@gmail.com>
2024-01-22 18:29:06 +0530

    linq

---
+++ Demo/API Basic Demo/5_C#_Advance/C#Advance/8_LINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using _8_LINQ.QueryExpression;
+
+namespace _8_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ - Query Exxpression");
+
+            QueryExpression.QueryExpression.QueryExpressionLINQ();
+
+
+        }
+    }
+}
---
+++ Demo/API Basic Demo/5_C#_Advance/C#Advance/8_LINQ/QueryExpression/QueryExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_LINQ.QueryExpression
+{
+    /// <summary>
+    /// Explains LINQ Query Expression
+    /// </summary>
+    static class QueryExpression
+    {
+        public static void QueryExpressionLINQ()
+        {
+            // Specify the data source.
+            int[] scores = { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query.
+            foreach (var i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
