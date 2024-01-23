191b75dfa77a4189dcbac01e29325068934b965d
yalathiya <yashlathiya59ppsv@gmail.com>
2024-01-23 17:22:44 +0530

    Dynamic Type

---
+++ Demo/API Basic Demo/5_C#_Advance/C#Advance/11_Dynamic/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _11_Dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dynamic avoids compile time checking
+
+            dynamic d = 1;
+            var testSum = d + 3;
+            
+            Console.WriteLine(testSum); // Here testSum is dynamic
+
+            dynamic d1 = 7;
+            dynamic d2 = "Mahendra Singh Dhoni";
+            dynamic d3 = System.DateTime.Today;
+
+            int i = d1;
+            string str = d2;
+            DateTime dt = d3;
+
+            Console.WriteLine(i);
+            Console.WriteLine(str);
+            Console.WriteLine(dt);
+        }
+    }
+}
