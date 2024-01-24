2d6730ee54fcff185e2abc0d35beaa57fabbd56f
yalathiya <yashlathiya59ppsv@gmail.com>
2024-01-24 10:17:47 +0530

    Rsa Algorithm

--- Demo/API Basic Demo/5_C#_Advance/C#Advance/10_Cryptography/BL/AesAlgo.cs
+++ Demo/API Basic Demo/5_C#_Advance/C#Advance/10_Cryptography/BL/AesAlgo.cs
@@ -13,6 +13,12 @@ namespace _10_Cryptography.BL
     /// </summary>
     public static class AesAlgo
     {
+        #region Private Members
+
+        private static string HardcodedKey = "keykeykeykeykeykeykeykeykeykeyky";
+
+        #endregion
+
         #region Public Methods 
 
         /// <summary>
@@ -20,27 +26,35 @@ namespace _10_Cryptography.BL
         /// </summary>
         /// <returns> cipher text string </returns>
         public static string GetCipher()
-         {
-            // 128 bit plain text and key
+        {
             string plainText = "abcdefghijklmnopqrstuvwxyzabcdef";
-            string keyValue = "keykeykeykeykeykeykeykeykeykeyky";
 
-            // That are converted in byte array
+            // Convert plaintext and hardcoded key to byte arrays
             byte[] plain = Encoding.UTF8.GetBytes(plainText);
-            byte[] key = Encoding.UTF8.GetBytes(keyValue);
+            byte[] key = Encoding.UTF8.GetBytes(HardcodedKey);
 
             // Using Aes Cng class -> encryption is performed
             using (AesCng aes = new AesCng())
             {
                 aes.Key = key;
 
+                // Generate a random IV for each encryption
+                aes.GenerateIV();
+                byte[] iv = aes.IV;
+
+                // IV is included in the result for later decryption
                 using (ICryptoTransform encryptor = aes.CreateEncryptor())
                 {
                     // Getting cipher 
                     byte[] cipherBytes = encryptor.TransformFinalBlock(plain, 0, plain.Length);
 
-                    // Cipher is converted to string
-                    return Convert.ToBase64String(cipherBytes);
+                    // Combine IV and cipher text for storage/transmission
+                    byte[] result = new byte[iv.Length + cipherBytes.Length];
+                    Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                    Buffer.BlockCopy(cipherBytes, 0, result, iv.Length, cipherBytes.Length);
+
+                    // Combined result is converted to string
+                    return Convert.ToBase64String(result);
                 }
             }
         }
@@ -52,23 +66,27 @@ namespace _10_Cryptography.BL
         /// <returns> plaintext in string </returns>
         public static string DecryptCipherText(string cipherText)
         {
-            // Key 
-            string keyValue = "keykeykeykeykeykeykeykeykeykeyky";
-            byte[] key = Encoding.UTF8.GetBytes(keyValue);
+            // Convert hardcoded key to byte array
+            byte[] key = Encoding.UTF8.GetBytes(HardcodedKey);
 
             // Cipher is converted into byte
-            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            byte[] resultBytes = Convert.FromBase64String(cipherText);
 
-            // Deciphering is performed by suing AesCng class
+            // Separate IV and cipher text
+            byte[] iv = resultBytes.Take(16).ToArray();
+            byte[] cipherBytes = resultBytes.Skip(16).ToArray();
+
+            // Deciphering is performed by using AesCng class
             using (AesCng aes = new AesCng())
             {
                 aes.Key = key;
+                aes.IV = iv;
 
                 using (ICryptoTransform decryptor = aes.CreateDecryptor())
                 {
                     // getting plain text 
                     byte[] plain = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
-                    
+
                     // Plain text in string format
                     return Encoding.UTF8.GetString(plain);
                 }
