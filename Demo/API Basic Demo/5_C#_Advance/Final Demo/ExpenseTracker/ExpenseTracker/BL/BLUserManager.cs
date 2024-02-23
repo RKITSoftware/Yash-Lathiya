d4c9acfe53c15db8f7c9c3d8a0df0d93bf5eeeb2
yalathiya <yashlathiya59ppsv@gmail.com>
2024-02-23 16:04:20 +0530

    Email functionality implemented

--- Demo/API Basic Demo/5_C#_Advance/Final Demo/ExpenseTracker/ExpenseTracker/BL/BLUserManager.cs
+++ Demo/API Basic Demo/5_C#_Advance/Final Demo/ExpenseTracker/ExpenseTracker/BL/BLUserManager.cs
@@ -10,6 +10,10 @@ using System.Net;
 using System.Security.Claims;
 using System.Text;
 using System.Text.RegularExpressions;
+using Google.Protobuf.WellKnownTypes;
+using Org.BouncyCastle.Asn1.Cmp;
+using Org.BouncyCastle.Asn1.Crmf;
+using ServiceStack;
 
 namespace ExpenseTracker.BL
 {
