9a646a1265013d0b3ce671450080ce2f103e0f50
yalathiya <yashlathiya59ppsv@gmail.com>
2024-04-02 17:42:01 +0530

    Exception Handling

---
+++ Demo/Dot Net Core/4_Exception_Handling/ExceptionHandling/ExceptionHandling/Controllers/CLTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExceptionHandling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CLTest : ControllerBase
+    {
+        [HttpGet]
+        [Route("TestException")]
+        public IActionResult TestException()
+        {
+            int a = 5;
+            int b = 0;
+            return Ok(a / b);
+        }
+    }
+}
---
+++ Demo/Dot Net Core/4_Exception_Handling/ExceptionHandling/ExceptionHandling/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ExceptionHandling
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                // Added developer exception page
+                // consists original line of code 
+                //app.UseDeveloperExceptionPage();
+
+                // Added ExceptionHandler Page 
+                // Content is delivered to web browser
+                // It is used in production level, where original lines of code should not be visible to users.
+                app.UseExceptionHandler( (options) =>
+                {
+                    options.Run(async (context) =>
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "text/html";
+                        var ex = context.Features.Get<IExceptionHandlerFeature>();
+                        if (ex != null)
+                        {
+                            string err = "<h1>This is custom error from Exception Handler</h1>" + 
+                                          "<h2>Error Message :" + ex.Error.Message + "</h2>" + 
+                                          "<h5>Error Stack" + ex.Error.StackTrace + "</h5>";
+                            await context.Response.WriteAsync(err);
+                        }
+                    });
+                });
+            }
+
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
