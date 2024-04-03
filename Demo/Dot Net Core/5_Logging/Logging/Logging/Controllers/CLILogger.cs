e188698240da4653c265c3171806d38f0deeb717
yalathiya <yashlathiya59ppsv@gmail.com>
2024-04-03 10:59:57 +0530

    Logging

---
+++ Demo/Dot Net Core/5_Logging/Logging/Logging/Controllers/CLILogger.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Logging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CLILogger : ControllerBase
+    {
+        private readonly ILogger<CLILogger> _logger;
+
+        public CLILogger(ILogger<CLILogger> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("LogInfoInConsole")]
+        public IActionResult LogInfoInConsole()
+        {
+            _logger.LogInformation("This is an information message");
+
+            _logger.LogWarning("This is a warning message");
+
+            _logger.LogError("This is an error message");
+
+            return Ok("Information logged successfully .. ");
+        }
+    }
+}
---
+++ Demo/Dot Net Core/5_Logging/Logging/Logging/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Logging
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                // Added developer exception page
+                app.UseDeveloperExceptionPage();
+
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
