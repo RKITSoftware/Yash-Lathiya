7fcc6e181111498e4dd8ae3d3c93a0702c2bd547
yalathiya <yashlathiya59ppsv@gmail.com>
2024-06-13 16:27:17 +0530

    radio group

--- Demo/DevExtreme/2_Editors/script/components/radioGrp.js
+++ Demo/DevExtreme/2_Editors/script/components/radioGrp.js
@@ -1,4 +1,59 @@
 export default function showRadioGrp() {
   $("#content").remove();
-  $(".container").append("<div id='content'>Im radio group</div>");
+  $(".container").append("<div id='content'></div>");
+
+  $("#content").append(
+    `<div class='left-right'>
+      <div class='left'>Meeting Priority (Vertical)</div>
+      <div class='right' id='vertical'></div>
+    </div>`
+  );
+
+  $("#content").append(
+    `<div class='left-right'>
+      <div class='left'>Meeting Priority (Horizontal)</div>
+      <div class='right' id='horizontal'></div>
+    </div>`
+  );
+
+  $("#content").append(
+    `<div class='left-right'>
+      <div class='left'>Confirmed Priority</div>
+      <div class='right' id='confirmed'></div>
+    </div>`
+  );
+
+  const priorities = ["Low", "Normal", "Urgent", "High"];
+
+  const vertical = $("#vertical")
+    .dxRadioGroup({
+      items: priorities,
+      value: priorities[0],
+      onValueChanged: (e) => {
+        // console.log(e.value);
+        confirmed.option("value", e.value);
+      },
+      itemTemplate: (itemData, _, itemElement) => {
+        console.log("itemData", itemData);
+        console.log("itemElement", itemElement);
+        itemElement.parent().addClass(itemData.toLowerCase()).text(itemData);
+      },
+    })
+    .dxRadioGroup("instance");
+
+  $("#horizontal").dxRadioGroup({
+    items: priorities,
+    value: priorities[0],
+    layout: "horizontal",
+    onValueChanged: (e) => {
+      console.log(e.value);
+    },
+  });
+
+  const confirmed = $("#confirmed")
+    .dxTextArea({
+      readOnly: true,
+      value: vertical.option("value"),
+    })
+    .dxTextArea("instance");
 }
