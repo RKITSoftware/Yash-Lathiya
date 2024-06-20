ad023c62b5dc93d4115d207147abed0eaa1c3c67
yalathiya <yashlathiya59ppsv@gmail.com>
2024-06-20 10:24:20 +0530

    custom store

--- Demo/DevExtreme/4_Data_Layer/script/components/customStore.js
+++ Demo/DevExtreme/4_Data_Layer/script/components/customStore.js
@@ -5,13 +5,10 @@ export default function showCustomStore() {
   $("#content").append(
     `<div id='heading'></div>
     <div class='row'>
-      <div id='selectBox'></div>
-    </div>
-    <div class='row'>
       <div id='byKey'></div>
     </div>
     <div class='row'>
-      <div id='clear'></div>
+      <div id='clearRowDataCache'></div>
     </div>
     <div class='row'>
       <div id='createQuery'></div>
@@ -52,11 +49,13 @@ export default function showCustomStore() {
   var store = new DevExpress.data.CustomStore({
     key: "id",
 
-    // loadMode: "row",
+    loadMode: "row", // accepted value -> row & processed
 
     // cacheRowData: true,
 
     loadMode: "processed", // default value => processed , accepted value => processed, row
+
+    useDefaultSearch: true,
 
     byKey: (key) => {
       var d = $.Deferred();
@@ -69,7 +68,10 @@ export default function showCustomStore() {
     },
 
     load: function (loadOptions) {
-      // ... custom load implementation
+      return $.ajax({
+        url: "https://jsonplaceholder.typicode.com/todos",
+        method: "GET",
+      });
     },
 
     insert: function (values) {
@@ -98,36 +100,66 @@ export default function showCustomStore() {
     onInserting: (values) => {
       console.log("*inserting..", "values", values);
     },
+
     onInserted: (values, key) => {
       console.log("*inserted..", "key", key, "values", values);
     },
+
     onLoaded: (result) => {
       console.log("*loaded..", "result", result);
     },
+
     onLoading: (loadOptions) => {
       console.log("*loading..", "loadOptions", loadOptions);
     },
+
     onModified: () => {
       console.log("*modified..");
     },
+
     onModifying: () => {
       console.log("*modifying..");
     },
-    onPush: () => {
-      console.log("*push");
+
+    onPush: (inputArr) => {
+      console.log("*push", "inputArr", inputArr);
+
+      if (inputArr[0].type == "insert") {
+        console.log("fetched insert");
+        const dataToAdded = inputArr[0].data;
+        store.insert(dataToAdded);
+      } else if (inputArr[0].type == "update") {
+        console.log("fetched update");
+        const keyToUpdate = inputArr[0].key;
+        const updatedData = inputArr[0].data;
+        store.update(keyToUpdate, updatedData);
+      } else if (inputArr[0].type == "remove") {
+        console.log("fetched remove");
+        const keyToRemove = inputArr[0].key;
+        store.remove(keyToRemove);
+      }
     },
+
     onRemoving: (key) => {
       console.log("*removing..", "key", key);
     },
+
     onRemoved: (key) => {
       console.log("*removed..", "key", key);
     },
+
     onUpdated: (key, values) => {
       console.log("*updated..", "key", key, "values", values);
     },
+
     onUpdating: (key, values) => {
       console.log("*updating..", "key", key, "values", values);
     },
+
+    totalCount: () => {
+      return 200;
+    },
+
     errorHandler: (err) => {
       console.log("err", err);
     },
@@ -144,6 +176,120 @@ export default function showCustomStore() {
         .fail((err) => {
           console.log("fail", err);
         });
+    },
+  });
+
+  $("#clearRowDataCache").dxButton({
+    text: "Clear Row Data Cache",
+    onClick: () => {
+      store.clearRawDataCache();
+      console.log("cleared cache");
+    },
+  });
+
+  $("#insert").dxButton({
+    text: "Insert 4 Gujarat Gandhinagar into store",
+    onClick: () => {
+      store
+        .insert({
+          id: 4,
+          state: "Gujarat",
+          capital: "Gandhinagar",
+        })
+        .done((dataObj, key) => {
+          console.log(
+            "**done | data inserted ",
+            "dataObj",
+            dataObj,
+            "key",
+            key
+          );
+        })
+        .fail(() => {
+          console.log("**fail | data insertion failed");
+        });
+    },
+  });
+
+  $("#key").dxButton({
+    text: "Key",
+    onClick: () => {
+      var keyProps = store.key();
+      console.log("keyProps", keyProps);
+    },
+  });
+
+  $("#keyOf").dxButton({
+    text: "keyOf id 2",
+    onClick: () => {
+      var key = store.keyOf({
+        id: 2,
+        state: "Alaska",
+        capital: "Juneau",
+      });
+      console.log("key", key);
+    },
+  });
+
+  $("#load").dxButton({
+    text: "load",
+    onClick: () => {
+      store.load();
+    },
+  });
+
+  $("#push").dxButton({
+    text: "Push",
+    onClick: () => {
+      console.log("******** push = insert ********");
+      store.push([
+        {
+          type: "insert",
+          data: { id: 4, state: "Gujarat", capital: "Gandhinagar" },
+          index: 4,
+        },
+      ]);
+      console.log("******** push = update ********");
+      store.push([
+        {
+          type: "update",
+          data: { id: 4, state: "Gujarat", capital: "Surat" },
+          key: 4,
+        },
+      ]);
+      console.log("******** push = remove ********");
+      store.push([
+        {
+          type: "remove",
+          key: 4,
+        },
+      ]);
+    },
+  });
+
+  $("#remove").dxButton({
+    text: "Remove",
+    onClick: () => {
+      store.remove(3); // parameter => key
+    },
+  });
+
+  $("#totalCount").dxButton({
+    text: "Total",
+    onClick: () => {
+      store.totalCount().done((count) => console.log(count));
+    },
+  });
+
+  $("#update").dxButton({
+    text: "Upadte 1 Delhi New Delhi",
+    onClick: () => {
+      store
+        .update(1, {
+          state: "Delhi",
+          capital: "New Delhi",
+        })
+        .done(() => {});
     },
   });
 }
