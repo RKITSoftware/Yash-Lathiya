5c829348b810c14897bf4ddc7f8b28ffb6f0de21
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2024-07-15 07:46:54 +0000

    DataGrid Binding

--- Demo/DevExtreme/5_Data_Grid/script/components/dataBinding.js
+++ Demo/DevExtreme/5_Data_Grid/script/components/dataBinding.js
@@ -2,8 +2,19 @@ export default function showDataBinding() {
   $("#content").remove();
   $(".container").append("<div id='content'></div>");
 
-  $("#content").dxDataGrid({
-    store: ["a", "b", "c"],
-    
-  });
+  $.ajax({
+    url : "../script/data/employees.json",
+    method : "GET",
+    dataType : "json",
+    success : (employees) => {
+      $("#content").dxDataGrid({
+        dataSource : employees,
+        keyExpr : "EmployeeID"
+      });
+    },
+    error : () => {
+      console.log("error to fetch employees from ajax request")
+    }
+  })
+  
 }
