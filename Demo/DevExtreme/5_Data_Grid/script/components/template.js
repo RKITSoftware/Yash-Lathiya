1698aa69d6adb3a3dbc223ced035fd6c5cf27ca7
Yash Lathiya <yashlathiya59ppsv@gmail.com>
2024-07-19 07:07:42 +0000

    template

--- Demo/DevExtreme/5_Data_Grid/script/components/template.js
+++ Demo/DevExtreme/5_Data_Grid/script/components/template.js
@@ -7,23 +7,43 @@ export default function showTemplate() {
       method : "GET",
       dataType : "json",
       success : (employees) => {
-        $("#content").dxDataGrid({
+        let datagrid = $("#content").dxDataGrid({
           dataSource : employees,
           keyExpr : "EmployeeID",
           paging: {
-            pageSize: 10,
+            pageSize: 5,
           },
           showColumnLines : true,
           showRowLines :true,
           rowAlternationEnabled : true,
           showBorders : true,
+          
+          // row customization
+          dataRowTemplate(container, item) {
+            const { data } = item;
+            const markup = '<tr class=\'main-row\' role=\'row\'>'
+          + `<td rowspan='2' role="gridcell"><img src='${data.Photo}' alt='Picture of ${data.FirstName} ${data.LastName}' tabindex='0'/></td>`
+          + `<td role='gridcell'>${data.EmployeeID}</td>`
+          + `<td role='gridcell'>${data.FullName}</td>`
+          + `<td role='gridcell'>${data.Position}</td>`
+          + `<td role='gridcell'>${data.TitleOfCourtesy}</td>`
+          + `<td role='gridcell'>${formatDate(new Date(data.BirthDate))}</td>`
+          + `<td role='gridcell'>${formatDate(new Date(data.Country))}</td>`
+      + '</tr>'
+      + '<tr class=\'notes-row\' role=\'row\'>'
+          + `<td colspan='6' role='gridcell'><div>Customized Row Template</div></td>`
+      + '</tr>';
+
+      container.append(markup);
+          },
+
+          // cell  customization
           onCellPrepared(options){
             const fieldData = options.value
-            // console.log(fieldData)
-            if(fieldData && fieldData.value){
-              fieldData.value("*" + fieldData.value) 
+            // console.log(options.cellElement)
+            if(fieldData){
+              options.cellElement.append("<span>*</span>")
             }
-            console.log(options)
           },
           columns : [
             {
@@ -36,7 +56,7 @@ export default function showTemplate() {
             },
             {
               dataField : "Position"
-            },+
+            },
             {
               dataField : "TitleOfCourtesy",
             },
@@ -50,6 +70,7 @@ export default function showTemplate() {
               width: 60,
               allowFiltering: false,
               allowSorting: false,
+              visibleIndex : 0,
               cellTemplate(container, options) {
                 $('<div>')
                 .append($('<img>', { src: options.value, alt: `Picture of ${options.data.FirstName} ${options.data.LastName}`, width : 50 }))
@@ -60,7 +81,8 @@ export default function showTemplate() {
               dataField : "Country",
             }
           ],
-        });
+        })
+          .dxDataGrid("instance");
       },
       error : () => {
         console.log("error to fetch employees from ajax request")
